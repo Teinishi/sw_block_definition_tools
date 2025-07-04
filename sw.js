11742cdd700b3489f2c47d02470fb1c32a77aeb2
Teinishi <79356047+Teinishi@users.noreply.github.com>
2025-07-04 09:43:13 +0000

    Deploying to gh-pages from @ Teinishi/sw_block_definition_tools@eadfc1d4fa7a24aff99f169c7bf09530d32ae126 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'sw-block-definition-tools-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './sw_block_definition_tools.js',
+  './sw_block_definition_tools_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
